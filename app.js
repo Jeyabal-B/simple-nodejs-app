e5446f809f6439e2a9b401cfb8ff1c392cab08e5
Jeyabal <jeyabal1993@gmail.com>
2022-06-05 20:50:09 +0530

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require(`http`)
+const express = require(`express`)
 const hostname = 'localhost';
 const port = `3000`
+var app = express();
 
 const server = http.createServer((req,res) => {
     res.statusCode = 200
@@ -8,6 +10,11 @@ const server = http.createServer((req,res) => {
     res.end(`This is a simple nodejs application running on the port:${port}`)
 })
 
-server.listen(port, hostname, () => {
+app.get(`/first`, function(req, res) {
+    var result = JSON.stringify({status:200, message:"First Node JS Application"})
+    res.send(result)
+})
+
+app.listen(port, function(){
     console.log(`Server is now running in the port:${port}`)
 })
